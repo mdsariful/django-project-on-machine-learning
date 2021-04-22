08fd01d7a428b19af6a829e81b495c329ef34d4c
mdsariful <sarif2435@gmail.com>
2021-04-22 14:34:59 +0800

    return

--- urls.py
+++ urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
  path('predict', views.predictdisease, name="predictdisease"),
  path('team', views.team, name="team"),
  path('predict', views.predict, name="predict"),
- path('about', views.about, name="about2"),
+ path('about', views.about, name="about"),
 
 ]
